2c34b260f9c7f635c52f65c598e22656df0a46ad
enisberk <EnisBerk@users.noreply.github.com>
2019-08-03 18:24:58 -0400

    add params file

---
+++ src/params.py
@@ -0,0 +1,9 @@
+
+EXCERPT_LENGTH=10 #seconds
+
+
+embedding_checkpoint
+VGGish_EMBEDDING_CHECKPOINT="assets/vggish_model.ckpt"
+PCA_PARAMS="assets/vggish_pca_params.npz"
+
+LABELS='assets/class_labels_indices.csv'
